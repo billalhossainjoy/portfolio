2a9180df5900c22b6440fee4e746fc04c65269ba
billalhossainjoy <billalhossain.bhj@gmail.com>
2025-06-16 21:04:34 +0600

    update links

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -5,7 +5,7 @@ import { TbBrandNextjs } from "react-icons/tb";
 import {
     SiNestjs, SiPrisma, SiMongodb, SiExpress, SiApollographql,
     SiExpo, SiReactquery, SiPusher, SiSocketdotio, SiCloudinary,
-    SiArduino, SiShadcnui
+    SiArduino, SiShadcnui, SiNginx, SiJest, SiRedis, SiGithubactions, SiRedux
 } from "react-icons/si";
 import { GrGraphQl } from "react-icons/gr";
 import { DiPostgresql } from "react-icons/di";
@@ -26,9 +26,11 @@ export const frontendLibraries: SkillIconOptions[] = [
     { label: "Tailwind CSS", icon: BiLogoTailwindCss, background: "#06B6D4", url: "https://tailwindcss.com/" },
     { label: "React Query", icon: SiReactquery, background: "#FF4154", url: "https://tanstack.com/query/latest" },
     { label: "Shadcn UI", icon: SiShadcnui, background: "#000000", color: "white", url: "https://ui.shadcn.com/" },
+    { label: "Jest", icon: SiJest, background: "#C21325", color: "white", url: "https://jestjs.io/" },
     { label: "Next.js", icon: TbBrandNextjs, background: "#000000", color: "white", url: "https://nextjs.org/" },
     { label: "TypeScript", icon: BiLogoTypescript, background: "#3178C6", url: "https://www.typescriptlang.org/" },
     { label: "Expo", icon: SiExpo, background: "#000020", color: "white", url: "https://expo.dev/" },
+    { label: "Redux", icon: SiRedux, background: "#764ABC", color: "white", url: "https://redux.js.org/" },
 ];
 
 export const backendLibraries: SkillIconOptions[] = [
@@ -38,13 +40,16 @@ export const backendLibraries: SkillIconOptions[] = [
     { label: "NestJS", icon: SiNestjs, background: "#E0234E", url: "https://nestjs.com/" },
     { label: "TypeScript", icon: BiLogoTypescript, background: "#3178C6", url: "https://www.typescriptlang.org/" },
     { label: "Apollo GraphQL", icon: SiApollographql, background: "#311C87", url: "https://www.apollographql.com/" },
+    { label: "Nginx", icon: SiNginx, background: "#009639", color: "white", url: "https://www.nginx.com/" },
     { label: "GraphQL", icon: GrGraphQl, background: "#E10098", url: "https://graphql.org/" },
+    { label: "Jest", icon: SiJest, background: "#C21325", color: "white", url: "https://jestjs.io/" },
     { label: "Prisma", icon: SiPrisma, background: "white", url: "https://www.prisma.io/" },
 ];
 
 export const databases: SkillIconOptions[] = [
     { label: "MongoDB", icon: SiMongodb, background: "#47A248", url: "https://www.mongodb.com/" },
     { label: "PostgreSQL", icon: DiPostgresql, background: "#336791", color: "white", url: "https://www.postgresql.org/" },
+    { label: "Redis", icon: SiRedis, background: "#DC382D", color: "white", url: "https://redis.io/" }
 ];
 
 export const othersLibraries: SkillIconOptions[] =  [
@@ -55,4 +60,5 @@ export const othersLibraries: SkillIconOptions[] =  [
     { label: "Cloudinary", icon: SiCloudinary, background: "#3448C5", color: "white", url: "https://cloudinary.com/" },
     { label: "Arduino", icon: SiArduino, background: "#00979D",color: "white", url: "https://www.arduino.cc/" },
     { label: "Git", icon: FaGitAlt, background: "#F05032", url: "https://git-scm.com/" },
+    { label: "CI/CD", icon: SiGithubactions, background: "#2088FF", color: "white", url: "https://github.com/features/actions" }
 ];
